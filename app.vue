a6153ff3ce0597a5e59f37417cc13b5d1e824c32
Nurdaulet Aldamzhar <aldamzharnurdaulet007@gmail.com>
2025-06-04 23:50:01 +0500

    Add Google Analytics script

--- app.vue
+++ app.vue
@@ -3,3 +3,22 @@
     <NuxtPage />
   </div>
 </template>
+
+<script setup>
+useHead({
+  script: [
+    {
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-K8CXRRNFLL',
+      async: true
+    },
+    {
+      innerHTML: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-K8CXRRNFLL');
+      `
+    }
+  ]
+})
+</script>
