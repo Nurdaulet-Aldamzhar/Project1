689483a6a73ce61a484451e29bc45278b4c6fdfb
Nurdaulet Aldamzhar <aldamzharnurdaulet007@gmail.com>
2025-06-05 00:31:04 +0500

    Fix meta tag configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: 'Bratva - Студенческая команда' }
+        { name: 'description', content: 'Bratva - Студенческая команда' }
       ],
       script: [
         {
